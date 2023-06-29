f4e2940230f5155291e24a3f1f1f3411c979b0eb
尚静静 <1587199415@qq.com>
2023-06-29 17:02:56 +0800

    删除部分文件

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ import os
 import time
 
 if __name__ == '__main__':
-    # pytest.main(["testsuites",
-    #              "--alluredir", "reports/allure_results"])
-    os.system("hrun testsuites/applyRecord/  --alluredir=reports/allure_results --clean-alluredir")
+    print("******************开始批量执行测试用例******************")
+    os.system("hrun testsuites/applyRecord/entApply-ent.yml --alluredir=reports/allure_results")
+    os.system("hrun testsuites/applyRecord/entApply-admin.yml --alluredir=reports/allure_results")
     time.sleep(1)
     os.system(r"allure generate --clean reports/allure_results -o reports/allure_reports")
