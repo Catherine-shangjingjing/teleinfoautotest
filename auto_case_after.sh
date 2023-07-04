deeb95381551e8cbdc2dab72618f66be94b4e189
尚静静 <1587199415@qq.com>
2023-07-04 18:17:22 +0800

    更新文件

--- auto_case_after.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-hmake testsuites/ysc/update/update.yml
-#pytest testsuites/ysc/update/update_test.py --alluredir=reports/allure_results --clean-alluredir
-#allure generate --clean reports/allure_results -o reports/allure_reports
-echo "******************测试用例全量执行完毕******************"
--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import time
 
 if __name__ == '__main__':
     print("******************开始批量执行测试用例******************")
-    os.system("hrun testsuites/applyRecord/entApply-ent.yml --alluredir=reports/allure_results")
-    os.system("hrun testsuites/applyRecord/entApply-admin.yml --alluredir=reports/allure_results")
+    os.system("hrun testcases/baidu.yml --alluredir=reports/allure_results")
+    os.system("hrun testsuites/baidu_test.yml --alluredir=reports/allure_results")
     time.sleep(1)
     os.system(r"allure generate --clean reports/allure_results -o reports/allure_reports")
